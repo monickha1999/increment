a20d1536f1f91f1eac506fd04eac3b1d12ef7633
monickha1999 <40392288+monickha1999@users.noreply.github.com>
2022-05-27 21:41:46 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+import IncDecCounter from "./IDcounter/IncDecCounter";
+function App() {
+  render(){
+  return (
+    <IncDecCounter />
+  );
+  }
+}
+export default App;
